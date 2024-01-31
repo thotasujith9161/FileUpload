a96b9c7202f6b613394fa292ef0c8ad38e6fbda8
Sujith <tksujith9898@gmail.com>
2024-01-30 23:40:20 -0600

    Added Project Files

---
+++ src/main/java/com/example/uploadingfiles/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.uploadingfiles.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	  @Bean
+	  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	    http
+	      .authorizeHttpRequests((authorize) -> authorize
+	        .anyRequest().fullyAuthenticated()
+	      )
+	      .formLogin(Customizer.withDefaults())
+	      .csrf(AbstractHttpConfigurer::disable);
+	    
+
+	    return http.build();
+	  }
+	  
+//	  @Bean
+//	  public LdapTemplate ldapTemplate() {
+//		  return new LdapTemplate(contextSource());
+//	  }
+	  
+//	  @Bean
+//	  public LdapContextSource contextSource() {
+//		  
+//		  LdapContextSource ldapContextSource = new LdapContextSource();
+//		  ldapContextSource.setUrl("ldap://localhost:10389");
+////		  ldapContextSource.setUserDn("uid=admin,ou=system");
+////		  ldapContextSource.setPassword("secret");
+//		  return ldapContextSource;
+//	}
+	  
+	 @Bean
+	 AuthenticationManager authManager(BaseLdapPathContextSource source) {
+		 LdapBindAuthenticationManagerFactory factory = new LdapBindAuthenticationManagerFactory(source);
+		 factory.setUserDnPatterns("cn={0}");
+		 return factory.createAuthenticationManager();
+		 
+	 }
+}
